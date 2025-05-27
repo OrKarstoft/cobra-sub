89f3bc6a8df09e192347ee9a80c3d3f612073c83
Oliver Karstoft <orkarstoft@gmail.com>
2025-05-27 18:16:05 +0200

    MyCommand

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/spf13/cobra"
+
+func MyCommand(token string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "mycommand",
+		Short: "A command imported from another repository",
+		Long: "This command is a part of a larger application and is imported from another repository.",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Println("Running mycommand with token:", token)
+		},
+	}
+
+	return cmd
+}
