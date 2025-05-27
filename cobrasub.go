e2f099a36d038805769321de7a22e5b5ebad1ea7
Oliver Karstoft <orkarstoft@gmail.com>
2025-05-27 20:30:53 +0200

    Adding sub sub

--- cobrasub.go
+++ cobrasub.go
@@ -3,7 +3,7 @@ package cobrasub
 import "github.com/spf13/cobra"
 
 func MyCommand(token string) *cobra.Command {
-	return &cobra.Command{
+	myCmd := &cobra.Command{
 		Use: "mycommand",
 		Short: "A command imported from another repository",
 		Long: "This command is a part of a larger application and is imported from another repository.",
@@ -11,4 +11,15 @@ func MyCommand(token string) *cobra.Command {
 			cmd.Println("Running mycommand with token:", token)
 		},
 	}
+
+	myCmd.AddCommand(&cobra.Command{
+		Use: "hello",
+		Short: "test",
+		Long: "test long",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Println("Hello with token:", token)
+		},
+	})
+
+	return myCmd
 }
